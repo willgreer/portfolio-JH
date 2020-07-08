af0ac60a6e41cd4467be3ea5a8f5cab4db849d1c
will greer <willfromspace@gmail.com>
2020-07-07 21:41:57 -0700

    fixed menus to bottom corners + misc style changes

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const page = document.querySelector("div.img-box");
 
 window.addEventListener('wheel', function(e) {
-
-    if (e.deltaY > 0) page.scrollLeft += 300;
-    else page.scrollLeft -= 300;
+    if (e.deltaY > 0) page.scrollLeft += 500;
+    else page.scrollLeft -= 500;
 });
