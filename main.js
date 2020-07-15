dd7f3b12aa213d204e1392a97c1de7252fee661b
will greer <willfromspace@gmail.com>
2020-07-14 21:29:16 -0700

    remove arrows

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ document.addEventListener('wheel', function (e) {
         : page.scrollLeft -= 1000;
 });
 
-arrowRight.addEventListener('click', function () {
-    page.scrollLeft += 1000;
-})
+// arrowRight.addEventListener('click', function () {
+//     page.scrollLeft += 1000;
+// })
 
-arrowLeft.addEventListener('click', function () {
-    page.scrollLeft -= 1000;
-})
+// arrowLeft.addEventListener('click', function () {
+//     page.scrollLeft -= 1000;
+// })
